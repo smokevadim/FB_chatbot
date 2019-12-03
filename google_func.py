1ddb347ecf082128439373def99e07707ff73176
Vadim Gazizov <nosmokingvadim@gmail.com>
2019-12-03 16:59:55 +0200

    in work

---
+++ google_func.py
@@ -0,0 +1,36 @@
+from googleapiclient.discovery import build
+from random import randint
+from tokens import *
+import requests
+
+def get_random_image(q=''):
+    results = google_search(q, GOOGLE_API_KEY, GOOGLE_CSE_ID, num=1, imgSize='small', imgType='photo', start=randint(1,30))
+    for result in results:
+        try:
+            for cse_image in result['pagemap']['cse_image']:
+                return cse_image['src']
+        except:
+            return "/ᐠ｡ꞈ｡ᐟ\\"
+
+def get_random_cat_image(q=''):
+    return(get_random_image('cute+cat+'+q))
+
+
+def google_search(search_term, api_key, cse_id, **kwargs):
+    service = build("customsearch", "v1", developerKey=api_key)
+    res = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
+    if int(res['searchInformation']['totalResults'])>0:
+        return res['items']
+    return []
+
+
+def download_image(url):
+    if 'http' in url:
+        req = requests.get(url)
+        with open('cat.jpg', 'wb') as f:
+            f.write(req.content)
+    return url
+
+if __name__ == '__main__':
+    download_image(get_random_cat_image('МЯУ'))
+
